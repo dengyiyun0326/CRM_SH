91fae30dcddaf12785810d50c0c38c83f5496011
Bruce Yang <tingya@microsoft.com>
2018-03-15 21:10:53 +0800

    version 1.0

---
+++ WebApi_OfficeServer_SH/Controllers/QueuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi_OfficeServer_SH.Models;
+
+namespace WebApi_OfficeServer_SH.Controllers
+{
+    public class QueuesController : ApiController
+    {
+        List<CaseQueue> caseQueueList = new List<CaseQueue>();
+
+        //initial test
+
+        // GET api/<controller>
+        public List<CaseQueue> Get()
+        {
+            return caseQueueList;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage Post([FromBody]List<CaseQueue> clientQueueList)
+        {
+
+            var response = Request.CreateResponse(HttpStatusCode.Created, "OfficeQueue");
+            return response;
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi_OfficeServer_SH/Models/CaseQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_OfficeServer_SH.Models
+{
+    public class CaseQueue
+    {
+        public string _caseID { get; set; }
+        public string _severity { get; set; }
+        public string _endTimeSLA { get; set; }
+        public string _callingCountry { get; set; }
+        public string _internalTitle { get; set; }
+        public string _customerTitle { get; set; }
+        public string _supportTopic { get; set; }
+        public string _belongQueue { get; set; }
+
+        public CaseQueue(string caseid, string severity, string endtimesla, string callingcountry,
+            string internaltitle, string customertitle, string supporttopic, string belongqueue)
+        {
+            _caseID = caseid;
+            _severity = severity;
+            _endTimeSLA = endtimesla;
+            _callingCountry = callingcountry;
+            _internalTitle = internaltitle;
+            _customerTitle = customertitle;
+            _supportTopic = supporttopic;
+            _belongQueue = belongqueue;
+        }
+    }
+}
